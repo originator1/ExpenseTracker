c45eb30cc39613cf83f86fd2f009c3e51ac28a87
James Garinger <jkggaringer@gmail.com>
2021-11-29 17:30:05 -0600

    adding service worker functionality

--- routes/api.js
+++ routes/api.js
@@ -25,6 +25,7 @@ router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
       res.json(dbTransaction);
+      console.log(dbTransaction);
     })
     .catch(err => {
       res.status(404).json(err);
