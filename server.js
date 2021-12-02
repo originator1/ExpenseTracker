02cb0b69a9583301cb2fa5f15c4edbf9a27004a7
James Garinger <jkggaringer@gmail.com>
2021-12-01 23:14:25 -0600

    deploying

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/ExpenseTracker',
+  process.env.MONGODB_URI || 'mongodb://localhost/ExpenseCluster',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
